fe870f8dd52ee54704b3cf2db21fadbe8b8fc34c
KanaKarimata <kana.lt22@gmail.com>
2022-10-01 08:54:21 +0900

    Finish Part10 継承

--- src/example/com/hero/Hero.java
+++ src/example/com/hero/Hero.java
@@ -2,13 +2,20 @@ package example.com.hero;
 import example.com.matango.Matango;
 
 public class Hero {
-    String name = "ミナト";
+    public String name = "ミナト";
     int hp = 10;
 
     public void attack (Matango m) {
         System.out.println(this.name + "の攻撃！");
         m.hp -= 5;
         System.out.println("５ポイントのダメージを与えた！");
+    }
 
+    public void run() {
+        System.out.println(this.name + "は逃げ出した！");
+    }
+
+    public Hero() {
+        System.out.println("Heroのコンストラクタが動作");
     }
 }
--- src/example/com/main/Main.java
+++ src/example/com/main/Main.java
@@ -1,7 +1,13 @@
 package example.com.main;
 
+import example.com.hero.Hero;
+import example.com.superhero.SuperHero;
+
 public class Main {
     public static void main(String[] args) {
-
+//        Hero h = new Hero();
+//        h.run();
+        SuperHero sh = new SuperHero();
+//        sh.run();
     }
 }
